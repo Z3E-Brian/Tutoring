cf1237adb33485dd92ce4fb308b3dbaefb7fa59c
Brian Zeledón <111306278+Z3E-Brian@users.noreply.github.com>
2025-08-11 10:27:35 -0600

    Update: tuto 3

---
+++ Tutoring #3/Ejercicios#1/Ejercicios#1/Ejercicios#1.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+/*Ejercicio #1 */
+void sumarYModificar(int* ptr, int& ref) {
+    *ptr += ref;
+}
+
+/*Ejercicio #3 */
+void cambiarPuntero(int** ptr2ptr) {
+    static int nuevoValor = 50;
+    *ptr2ptr = &nuevoValor;
+}
+
+
+int main() {
+    //#1
+   int num1 = 10;
+   int num2 = 5;
+
+    cout << "Valor antes de modificar: " << num1 << endl;
+
+    sumarYModificar(&num1, num2); 
+
+    cout << "Valor despues de modificar: " << num1 << endl; 
+
+    /**************************************************************************************/
+    //#2
+    int a = 10, b = 20, c = 30;
+    int* arr[3];
+
+    arr[0] = &a;
+    arr[1] = &b;
+    arr[2] = &c;
+
+    for (int i = 0; i < 3; ++i) {
+        cout << "Valor de arr[" << i << "]: " << *arr[i] << ", Direccion: " << arr[i] << endl;
+    }
+
+    /**************************************************************************************/
+    //#3
+        int num = 20;
+    int* ptr = &num;
+
+    cout << "Antes de cambiar: " << *ptr << endl;
+
+    cambiarPuntero(&ptr);
+
+    cout << "Despues de cambiar: " << *ptr << endl;
+
+    /**************************************************************************************/
+    //#4
+
+    const char* cadenas[3] = { "Hola", "Mundo", "C++" };
+
+    for (int i = 0; i < 3; ++i) {
+        cout << "Cadena " << i << ": " << cadenas[i] << endl;
+    }
+
+    return 0;
+}
